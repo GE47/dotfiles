9e0a7ac07cc73f18d66b84026ea486e1dc524797
GE47 <hassan2pac47@gmail.com>
2021-11-06 01:11:25 +0300

    nvim: add tree-sitter-org grammer

--- nvim/.config/nvim/lua/lv-treesitter/init.lua
+++ nvim/.config/nvim/lua/lv-treesitter/init.lua
@@ -1,12 +1,24 @@
+local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+parser_config.org = {
+  install_info = {
+    url = 'https://github.com/milisims/tree-sitter-org',
+    revision = 'main',
+    files = {'src/parser.c', 'src/scanner.cc'},
+  },
+  filetype = 'org',
+}
+
 require'nvim-treesitter.configs'.setup {
     ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
     ignore_install = {"haskell"},
     highlight = {
-        enable = true -- false will disable the whole extension
+        enable = true, -- false will disable the whole extension
+        disable={'org'},
+        additional_vim_regex_highlighting = {'org'},
     },
 	rainbow = {
 	    enable = true,
-        disable = {"html"},
+        disable = {"html","javascriptreact"},
 	    extended_mode = true, -- Highlight also non-parentheses delimiters, boolean or table: lang -> boolean
 	    max_file_lines = 1000, -- Do not enable for files with more than 1000 lines, int
 	},
