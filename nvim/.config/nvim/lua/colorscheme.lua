4099560484c36518e55b43f5df61f4508038323c
GE47 <hassan2pac47@gmail.com>
2021-11-24 02:17:35 +0300

    nvim: make functions & keywords italic in `tokyonight` theme

--- nvim/.config/nvim/lua/colorscheme.lua
+++ nvim/.config/nvim/lua/colorscheme.lua
@@ -11,6 +11,8 @@
 -- tokyonight theme
 vim.g.tokyonight_style = 'night'
 vim.g.tokyonight_transparent= true
+vim.g.tokyonight_italic_functions=true
+vim.g.tokyonight_italic_keywords=true
 vim.g.tokyonight_lualine_bold= false
 vim.g.tokyonight_dark_sidebar = false
 vim.g.tokyonight_dark_float = true
