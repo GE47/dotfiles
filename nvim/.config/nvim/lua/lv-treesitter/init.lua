4821482a88a8a38a6635837f272804eb3c3737ee
GE47 <hassan2pac47@gmail.com>
2022-02-21 13:34:21 +0300

    fix(nvim): update orgmode treesitter parser setup

--- nvim/.config/nvim/lua/lv-treesitter/init.lua
+++ nvim/.config/nvim/lua/lv-treesitter/init.lua
@@ -1,12 +1,6 @@
 local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
-parser_config.org = {
-  install_info = {
-    url = 'https://github.com/milisims/tree-sitter-org',
-    revision = 'main',
-    files = {'src/parser.c', 'src/scanner.cc'},
-  },
-  filetype = 'org',
-}
+
+require('orgmode').setup_ts_grammar()
 
 require'nvim-treesitter.configs'.setup {
     ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
